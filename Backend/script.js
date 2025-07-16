f1d038b1d4e9d0f5ee941de24beb8668ab21d82d
HeyItsDizzy <bestrod1@gmail.com>
2025-07-16 11:49:16 +0200

    chore: sync Backend from VPS for v2

--- Backend/script.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require("bcryptjs");
-
-(async () => {
-  const plainPassword = "admin123";
-  const hashedPassword = "$2b$10$oE0vQNMSYTc5.7enEQ0Kuu0FR2XATxO9w3Juu.r7XFh33aLnzvK/W";
-
-  const isMatch = await bcrypt.compare(plainPassword, hashedPassword);
-  console.log("Password Match:", isMatch);
-})();
